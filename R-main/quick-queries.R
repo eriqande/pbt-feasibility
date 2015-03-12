5ae2d3d0b920b0ffd87aa4ac50ad79adeb9408ba
Eric C. Anderson <eric.anderson@noaa.gov>
2015-03-12 14:28:08 -0700

    add a quick queries file to get some numbers

---
+++ R-main/quick-queries.R
@@ -0,0 +1,19 @@
+
+
+
+# get the number of fish released in the wild with CWTs
+source("R-main/01-prepare-for-mcmc.R")  # source this to get the "releases" variable
+
+# order states as desired
+releases$release_location_state <- factor(releases$release_location_state, levels = c("AK", "BC", "WA", "ID", "OR", "CA"))
+
+# group it
+tmp <- releases %>%
+  filter(rearing_type %in% c("W", "M"), brood_year >= 2005, species %in% 1:2) %>%
+  group_by(species, release_location_state, brood_year, rearing_type) %>%
+  select(starts_with("n_"))
+
+# query and inner join results
+num_fish <-  tmp %>% summarise_each(funs(sum))
+num_releases <-  tmp %>% summarise(num_release_groups = n())
+inner_join(num_releases, num_fish)
